34999006b97d42fca0b4afc797b833056b3816a4
YleiaYvette <yleiayvette@yleias-MacBook-Air.local>
2019-08-14 14:47:06 -0700

    Took out unnecessary if statement

--- Search.py
+++ Search.py
@@ -12,10 +12,7 @@ args=parser.parse_args()
 
 searchURL='https://www.encodeproject.org/matrix/?type=Experiment&status=released'
 
-control=True
-
 if args.target is not None: #If Not Control
-    control=False
     searchURL+='&target.label='+args.target
     searchURL+='&target.label%21=Control' #Target of Assay NOT Control
     searchURL+='&assay_title=TF+ChIP-seq'#Assay Title = TF ChIP-seq
@@ -30,17 +27,6 @@ searchURL+='&format=json' #Puts in json format
 
 with urllib.request.urlopen(searchURL) as page:
     page=json.loads(page.read().decode())
-    if control==False:
-        assayTitles=page['facets'][1]['terms'] 
-        TFAmt=0
-        for i in assayTitles:
-            assay=i.get('key')
-            if assay=='TF ChIP-seq':
-                TFAmt=i.get('doc_count') 
-                break
-        if TFAmt <= 0:
-            print('No experimenrs found for TF ChIP-seq. Try again')
-            SystemExit
     biosamples=page['facets'][9]['terms']
     for i in biosamples:
         biosample=i.get('key')
@@ -59,7 +45,7 @@ for line in txtFile:
     print(line)
 
 
-#page['facets'][0]['terms']=AssayTypes
+#page['facets'][0]['terms']=AssayTypesgit
 #page['facets'][1]['terms']=AssayTitles
 #page['facets'][2]['terms']=Status
 #page['facets'][3]['terms']=Project
