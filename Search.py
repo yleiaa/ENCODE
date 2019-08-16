31da83741eaa35e24044df13334635c4f8166584
YleiaYvette <yleiayvette@yleias-MacBook-Air.local>
2019-08-16 13:30:32 -0700

    Thread pool added

--- Search.py
+++ Search.py
@@ -4,6 +4,7 @@ import sys
 import json
 import argparse
 import urllib.request
+from time import process_time
 from multiprocessing.pool import ThreadPool
 
 def download(link):
@@ -18,20 +19,21 @@ searchURL='https://www.encodeproject.org/matrix/?type=Experiment&status=released
 
 if args.target is not None: #If Not Control
     searchURL+='&target.label='+args.target
-    searchURL+='&target.label%21=Control' #Target of Assay NOT Control
-    searchURL+='&assay_title=TF+ChIP-seq'#Assay Title = TF ChIP-seq
+    searchURL+='&target.label%21=Control'
+    searchURL+='&assay_title=TF+ChIP-seq'
 else: #If Control
-    searchURL+='&assay_title=Control+ChIP-seq' #Assay Title=Control ChIP-seq
-    searchURL+='&target.investigated_as=control' #Target Category (Same # of results as Assay Title)
-    searchURL+='&target.label=Control' #Target Label- Narrows it down a little to only human controls(?)
+    searchURL+='&assay_title=Control+ChIP-seq' 
+    searchURL+='&target.investigated_as=control'
+    searchURL+='&target.label=Control' 
 
-searchURL+='&biosample_ontology.term_name='+args.biosample #Biosample Term Name 
-searchURL+='&files.file_type=fastq'#Available File Types
-searchURL+='&format=json' #Puts in json format
+searchURL+='&biosample_ontology.term_name='+args.biosample 
+searchURL+='&files.file_type=fastq'
+searchURL+='&format=json'
 
 with urllib.request.urlopen(searchURL) as page:
     page=json.loads(page.read().decode())
     biosamples=page['facets'][9]['terms']
+    audits=page['facets'][14]['terms']
     for i in biosamples:
         biosample=i.get('key')
         if biosample==args.biosample:
@@ -56,8 +58,6 @@ for line in txtFile:
     elif lineNum>1:
         downloadLinks.append(line)
 
-with ThreadPool() as pool:
-    results=pool.map(download, downloadLinks)
 
 
 #page['facets'][0]['terms']=AssayTypesgit
